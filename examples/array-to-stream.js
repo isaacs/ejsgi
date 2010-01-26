597c79f84845ddc9fc224a98c804b91248e02997
isaacs <i@izs.me>
2010-01-26 02:24:39 -0800

    Middleware that provides an adapter for forEach-style JSGI apps.

---
+++ examples/array-to-stream.js
@@ -0,0 +1,29 @@
+
+// like hello world, but with a string and some handy middleware,
+// rather than with just a stream.
+
+var ejsgi = require("../lib/ejsgi"),
+  Stream = require("../lib/ejsgi/stream"),
+  helloApp = function (req) {
+    var out = {};
+    var message = ["Hel","lo,"," worl","d!"];
+    out.status = 200;
+    out.headers = {
+      "content-type" : "text/plain"
+    };
+    out.body = message;
+    return out;
+  },
+  foreachableToStream = function (app) { return function (req) {
+    var out = app(req);
+    if (typeof(out.body.forEach) === "function") {
+      var body = new Stream;
+      out.body.forEach(function (chunk) { body.write(chunk) });
+      out.body = body;
+    }
+    return out;
+  }};
+
+ejsgi.Server(foreachableToStream(helloApp), "localhost", 8000).start();
+
+require("sys").error("Running on http://localhost:8000/");
