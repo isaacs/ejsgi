52d8594c3eb94205c8539b740958ac75a1d62342
isaacs <i@izs.me>
2010-02-01 14:08:36 -0800

    Update the baseline server to send multiple headers, so that I can benchmark that in node vs putting it in the JSGI layer instead.

--- examples/baseline.js
+++ examples/baseline.js
@@ -1,7 +1,13 @@
 
 require("http").createServer(function (req,res) {
   var message = "hello, world";
-  res.sendHeader(200, {"content-type":"text/plain", "content-length":message.length});
+  res.sendHeader(200, {
+    "content-type":"text/plain",
+    "content-length":message.length,
+    "x-x" : [
+      "foo", "bar", "baz", "quux"
+    ]
+  });
   res.sendBody(message);
   res.finish();
 }).listen(8000, "localhost");
